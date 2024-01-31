1769ca79d5415670dd8325e117d78de39af560cf
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-01-31 14:30:35 +0530

    socket-added

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ import Connection from './database/db.js';
 import Router from './routes/Routes.js';
 import {Server} from "socket.io";
 
+const io = new Server(9000, {
+   cors:{
+        origin: "http://localhost:3000"
+    },
+})
+
 const app = express();
 dotenv.config()
 
@@ -14,48 +20,53 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/',Router)
 
-const PORT = 8000 ||process.env.PORT
+const PORT = 8000
 
 Connection()
+
+
+app.listen(PORT,()=>console.log('server is running on port',PORT) )
+
+
+// socket 
 let users = [];
 
-const server =app.listen(PORT,()=>console.log('server is running on port',PORT) )
-const io = new Server(9000, {
-    pingTimeout: 60000,
-    cors: {
-      origin: "http://localhost:3000",
-      // credentials: true,
-    },
-  });
- 
-
 const addUser = (userData, socketId) => {
+    // console.log('addUser function called with userData, socketId:', userData, socketId);
+    // console.log('User already exists:', users.some(user => user.sub === userData.sub));
     !users.some(user => user.sub === userData.sub) && users.push({ ...userData, socketId });
     
+    // console.log('users after adding user:', users);// getting value of users
 }
-
 const getUser = (userId) =>{
-  console.log('users in getuser func   -',users)
-    return users.find(user => user.sub === userId)
+    console.log('userId -- ',userId);
+    console.log('users-in-getUser-',users)//why not getting value of users
+
+    const user =  users.find(  user   => user.sub === userId)
+    console.log('Found user:', user);
+    return user;
+
 }
+io.on('connection', async (socket) => {
+    try {
+        console.log('user connected');
 
-io.on('connection',  (socket) => {
-    console.log('user connected')
-    
-    socket.on("addUser",userData =>{
-      addUser(userData,socket.id)
+        socket.on("addUser", async (userData) => {
+            await addUser(userData, socket.id);
 
-        console.log('active users-socket-in',users)
-        io.emit("getUsers",users)
-    })
-    console.log('active users',users)
-    
-    socket.on("sendMessage",data=>{
-    console.log('sendMessage data--',data)
-        
-        const user = getUser(data.receiverId);
-        console.log('user in socket - ',user)
-        // io.to(user.socketId).emit('getMessage',data)
-    })
-    
-})
+            console.log('active users-socket-in', users);
+            io.emit("getUsers", users);
+        });
+
+        console.log('active users', users);
+
+        socket.on("sendMessage", async (data) => {
+            const user = getUser(data.recieverId);
+            console.log('user in socket - ', user);
+            console.log('usersss in socket - ', users);
+            user && io.to(user.socketId).emit('getMessage', data);
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
